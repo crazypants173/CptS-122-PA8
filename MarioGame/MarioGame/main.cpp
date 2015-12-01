5494282f7b57d9606b39fb0d835387da29a56f03
crazypants173 <benherrmann950@gmail.com>
2015-12-01 14:34:29 -0800

    Included SFML Libraries - Config May Not Work Currently!

---
+++ MarioGame/MarioGame/main.cpp
@@ -0,0 +1,36 @@
+/*
+	Programming Assignment 8 - Mario Game
+	A Mario Emulator
+
+	Authors
+		Benjamin Herrmann
+		___
+		___
+		___
+*/
+
+#include <SFML\Config.hpp>
+#include <SFML\Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
