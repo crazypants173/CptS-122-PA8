1aa2dfa985c430d2980f6101d61b9111cf96c0a0
crazypants173 <benherrmann950@gmail.com>
2015-12-07 15:28:27 -0800

    Basic Network Implementation - Not perfect. At least sends packets - *Might* recieve them

--- MarioGame/MarioGame/main.cpp
+++ MarioGame/MarioGame/main.cpp
@@ -12,6 +12,7 @@
 #include <SFML/Graphics.hpp>
 #include "game.h"
 #include "Map.h"
+#include "gameNetworking.h"
 
 /*
 	To access main game window, type Game::getMainWin(); - returns a reference to the main window
@@ -22,43 +23,43 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+ //   sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+ //   sf::CircleShape shape(100.f);
+ //   shape.setFillColor(sf::Color::Green);
 
-	sf::View v(sf::FloatRect(0,0,800, 600));
-	window.setView(v);
-	
-	Map m(800,600);
-	m.load("test.map");
+	//sf::View v(sf::FloatRect(0,0,800, 600));
+	//window.setView(v);
+	//
+	//Map m(800,600);
+	//m.load("test.map");
 
-	Collision_Detection cd; 
-	cd.Load("img/Hero_duck2.png");
-	cd.SetPosition(0,300);
-	cd.Draw(window);
-	
+	//Collision_Detection cd; 
+	//cd.Load("img/Hero_duck2.png");
+	//cd.SetPosition(0,300);
+	//cd.Draw(window);
+	//
 
-	sf::Clock c;
+	//sf::Clock c;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+ //   while (window.isOpen())
+ //   {
+ //       sf::Event event;
+ //       while (window.pollEvent(event))
+ //       {
+ //           if (event.type == sf::Event::Closed)
+ //               window.close();
+ //       }
 
-		window.clear(sf::Color::Blue);
-		window.setView(v);
+	//	window.clear(sf::Color::Blue);
+	//	window.setView(v);
 
-		v.move(200.0 * c.getElapsedTime().asSeconds(), 0);
-		c.restart();
-		window.draw(m);
-		cd.Draw(window);
-        window.display();
-		
-    }
+	//	v.move(200.0 * c.getElapsedTime().asSeconds(), 0);
+	//	c.restart();
+	//	window.draw(m);
+	//	cd.Draw(window);
+ //       window.display();
+	//	
+ //   }
 
 	Game::StartGame();
 
